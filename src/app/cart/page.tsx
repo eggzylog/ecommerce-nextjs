ef8b1ad3db3914bb15f90b5a15ce544f53b2ae03
jvl <jovenvictorlogo@gmail.com>
2023-09-20 12:07:43 +0800

    added cart page

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,34 @@
+import { getCart } from '@/lib/db/cart';
+import CartEntry from './CartEntry';
+import { setProductQuantity } from './actions';
+import { formatPrice } from '@/lib/format';
+
+export const metadata = {
+  title: 'Your Cart - Ecommerce',
+};
+
+const CartPage = async () => {
+  const cart = await getCart();
+
+  return (
+    <div>
+      <h1 className='text-3xl mb-6 font-bold'>Shopping Cart</h1>
+      {cart?.items.map((cartItem) => (
+        <CartEntry
+          key={cartItem.id}
+          cartItem={cartItem}
+          setProductQuantity={setProductQuantity}
+        />
+      ))}
+      {!cart?.items.length && <p>Your cart is empty.</p>}
+      <div className='flex flex-col items-end sm:items-center'>
+        <p className='mb-3 font-bold'>
+          Total: {formatPrice(cart?.subtotal || 0)}
+        </p>
+        <button className='btn btn-primary sm:w-[200px]'>Checkout</button>
+      </div>
+    </div>
+  );
+};
+
+export default CartPage;
